79cf2d03ab926f6da64470a64982eb8f7378918e
Anh Dang <aqdang16@earlham.edu>
2018-10-29 22:12:03 -0400

    Update Assets/Code/movemaze.cs

--- Assets/Code/movemaze.cs
+++ Assets/Code/movemaze.cs
@@ -13,15 +13,19 @@ public class movemaze : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey("w")){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(0,-10);
+			heroObj.transform.eulerAngles = new Vector3 (0,0,0);
 		} else{
 			if (Input.GetKey("s")){
 				GetComponent<Rigidbody2D>().velocity = new Vector2(0,10);
+				heroObj.transform.eulerAngles = new Vector3 (0,0,180);
 			} else {
 				if (Input.GetKey("a")){
 					GetComponent<Rigidbody2D>().velocity = new Vector2(10,0);
+					heroObj.transform.eulerAngles = new Vector3 (0,0,90);
 				} else {
 					if (Input.GetKey("d")){
 						GetComponent<Rigidbody2D>().velocity = new Vector2(-10,0);
+						heroObj.transform.eulerAngles = new Vector3 (0,0,-90);
 					}
 					else{
 						GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
