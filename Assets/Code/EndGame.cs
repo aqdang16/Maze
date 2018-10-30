12fc3d219cad67a6588e76d04d33358f848eb6ab
dangtunglam98 <ltdang16@earlham.edu>
2018-10-29 23:06:00 -0400

    EndGame added

---
+++ Assets/Code/EndGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "pacman")
+        {
+            //Check if the isTrigger option on th Collider2D is set to true or false
+
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
