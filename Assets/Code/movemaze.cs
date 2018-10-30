45a4b1cf50e45c9449e732364c9b0261b97e03f3
Anh Dang <aqdang16@earlham.edu>
2018-10-29 22:09:59 -0400

    Initial commit

---
+++ Assets/Code/movemaze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movemaze : MonoBehaviour {
+	public Transform heroObj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0,-10);
+		} else{
+			if (Input.GetKey("s")){
+				GetComponent<Rigidbody2D>().velocity = new Vector2(0,10);
+			} else {
+				if (Input.GetKey("a")){
+					GetComponent<Rigidbody2D>().velocity = new Vector2(10,0);
+				} else {
+					if (Input.GetKey("d")){
+						GetComponent<Rigidbody2D>().velocity = new Vector2(-10,0);
+					}
+					else{
+						GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+					}
+				}
+			}
+		}
+	}
+}
